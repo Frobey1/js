0354acf8f65260f067ac88c75137333c750cdb93
Frobey1 <106011989+Frobey1@users.noreply.github.com>
2024-07-27 21:43:57 +0000

    conversion in js

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,2 +1,68 @@
 
 console.log("Froso");
+
+"use strict"; //treat all js code as newer version
+
+// aleart(3+3) // we are using node js not browser
+
+/*console.log(3+
+    3); // code readability should be prioratised // not a good practice to write like this
+console.log("Shubham") */
+
+
+
+
+let name = "Shubham"
+let age = 23
+let is_logged_in = false
+
+
+// number = 2 to the power of 53
+// bigint
+// strin in double quote or single quote
+// boolean = tru/false
+// null = stand alone value that is mean the value is strictly null
+// undefined =  when the value is not asigned
+// symbol = unique
+
+
+// object  
+
+
+//console.log(typeof age)  // number
+//console.log(typeof null)  // object
+//console.log(typeof undefined)  // undefined
+
+
+// conversion in numbers, boolean and string.
+
+
+let score =null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let value_in_number = Number(score)
+console.log(typeof value_in_number)
+console.log(value_in_number);
+
+
+//"33"=> 33
+//"33abs"=> NaN
+//null=> 0
+//boolean true or false=> 1 or 0
+
+
+let is_loggedin = 1;
+let boolean_is_loggedin = Boolean(is_loggedin);
+//console.log(boolean_is_loggedin)
+
+
+// when we leave the variable empty "" then the boolean gives the value of false and when we put any string then it is true. same with 1 and 0.
+
+let is_number = 33
+let string_is_number= String(is_number)
+console.log(typeof string_is_number)
+
+
+
